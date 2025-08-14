6cf8cea80f79a1352a8e8fa8b978129ad3932989
Amir Pasha Motamed <pasha@telos.net>
2025-08-14 19:05:30 +0330

    Enhance self-stake boost tests with multiplier checks and refactor boost setting logic

--- tests/self_stake_boost.py
+++ tests/self_stake_boost.py
@@ -1,7 +1,6 @@
 from antelope_rs.antelope_rs import Asset
 from voting_utils import vote_native, producers, assert_bp_voteweight, get_native_account_vote_weight
 
-
 def check_self_stake_boost(cleos, native_funder, manager_contract):
     amount = 1_000_000
     producer_names = producers[:30]
@@ -17,6 +16,28 @@ def check_self_stake_boost(cleos, native_funder, manager_contract):
     resource_amount = Asset.from_str(f"{half_amount}.0000 TLOS")
     cleos.delegate_bandwidth(producer_names[0], producer_names[0], resource_amount, resource_amount, False)
 
+    # Check with no boost
+    check_self_stake_boost_with_multiplier(cleos, native_funder, manager_contract, 0)
+    # Check with 7.5x boost (750%)
+    check_self_stake_boost_with_multiplier(cleos, native_funder, manager_contract, 750)
+    # # Check with 10x boost (1000%)
+    check_self_stake_boost_with_multiplier(cleos, native_funder, manager_contract, 1000)
+    # Reset to no boost
+    check_self_stake_boost_with_multiplier(cleos, native_funder, manager_contract, 0)
+
+    # Reset votes to 0
+    vote_native(cleos, producer_names[0], [])
+    assert_bp_voteweight(cleos, manager_contract, producer_names, 0)
+    cleos.logger.info(f"Reset votes to 0")
+
+
+def check_self_stake_boost_with_multiplier(cleos, native_funder, manager_contract, self_stake_boost_multiplier):
+    producer_names = producers[:30]
+
+    # Set self stake boost to 10x
+    set_self_stake_boost(cleos, manager_contract, self_stake_boost_multiplier)
+    cleos.logger.info(f"Set self stake boost to {self_stake_boost_multiplier/100}x")
+
     # Vote to BPs including itself
     vote_native(cleos, producer_names[0], producer_names)
     cleos.logger.info(f"Voted with {producer_names[0]} for producers: {producer_names}")
@@ -28,12 +49,11 @@ def check_self_stake_boost(cleos, native_funder, manager_contract):
     threshold = int(vote_weight * 0.001)  # 0.1% tolerance
 
     # Get the total votes of itself and check that it is boosted by 10x
-    self_stake_boost_multiplier = 10
     self_producer = cleos.get_producer(producer_names[0])
     self_total_votes = int(0)
     if self_producer:
         self_total_votes = int(float(self_producer['total_votes']))
-    assert abs(self_total_votes - vote_weight * (1 + self_stake_boost_multiplier)) <= threshold
+    assert abs(self_total_votes - vote_weight * (1 + self_stake_boost_multiplier/100)) <= threshold
 
     # Get the total votes of other BPs and check that they are not boosted
     for producer in producer_names[1:]:
@@ -43,6 +63,13 @@ def check_self_stake_boost(cleos, native_funder, manager_contract):
             other_total_votes = int(float(other_producer['total_votes']))
         assert abs(other_total_votes - vote_weight) <= threshold
 
-    # Reset votes to 0
-    vote_native(cleos, producer_names[0], [])
-    assert_bp_voteweight(cleos, manager_contract, producer_names, 0)
+def set_self_stake_boost(cleos, manager_contract, self_stake_boost_multiplier):
+    action_result = cleos.push_action(
+        'eosio',
+        'setselfstake',
+        [self_stake_boost_multiplier],
+        'eosio',
+        key=cleos.private_keys['eosio']
+    )
+    cleos.wait_evm_blocks(5)
+    cleos.logger.info(f"Set self stake boost")
